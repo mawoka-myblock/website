22a191a3d1342f07130b1fad40c609b5a17fa7f6
mawoka <mawoka-myblock@e.email>
2021-05-14 17:08:53 +0200

    Added some greetings

--- javascript/main.js
+++ javascript/main.js
@@ -5,11 +5,20 @@ function virus() {
 }
 
 const greetings = [
-    "<br><br>Hey! <span class='hand'>👋</span><p style='color: beige;font-size: 30%;font-weight: 400;'>How are you?</p><br><br>",
-    "<br><br>Moin! <span class='hand'>👋</span><p style='color: beige;font-size: 30%;font-weight: 400;'>How are you?</p><br><br>",
-    "<br><br>Hello! <span class='hand'>👋</span><p style='color: beige;font-size: 30%;font-weight: 400;'>How are you?</p><br><br>",
-    "<br><br>G\'day! <span class='hand'>👋</span><p style='color: beige;font-size: 30%;font-weight: 400;'>How are you?</p><br><br>",
-    "<br><br>Moin! <span class='hand'>👋</span><p style='color: beige;font-size: 30%;font-weight: 400;'>How are you?</p><br><br>"
+    "<br><br>Hey! <span class='hand'>👋</span><p style='color: beige;font-size: 30%;font-weight: 400;'>How are you?</p><br><br>", //Hey!
+    "<br><br>Moin! <span class='hand'>👋</span><p style='color: beige;font-size: 30%;font-weight: 400;'>How are you?</p><br><br>", //Moin!
+    "<br><br>Hello! <span class='hand'>👋</span><p style='color: beige;font-size: 30%;font-weight: 400;'>How are you?</p><br><br>", //G'day!
+    "<br><br>G\'day! <span class='hand'>👋</span><p style='color: beige;font-size: 30%;font-weight: 400;'>How are you?</p><br><br>", //Moin
+    "<br><br>Grüß Gott! <span class='hand'>👋</span><p style='color: beige;font-size: 30%;font-weight: 400;'>How are you?</p><br><br>", //Grüß Gott!
+    "<br><br>Merhaba! <span class='hand'>👋</span><p style='color: beige;font-size: 30%;font-weight: 400;'>How are you?</p><br><br>", //Merhaba!
+    "<br><br>Hi! <span class='hand'>👋</span><p style='color: beige;font-size: 30%;font-weight: 400;'>How are you?</p><br><br>", // Hi!
+    "<br><br>Yo! <span class='hand'>👋</span><p style='color: beige;font-size: 30%;font-weight: 400;'>How are you?</p><br><br>", //Yo!
+    "<br><br>Sup? <span class='hand'>👋</span><p style='color: beige;font-size: 30%;font-weight: 400;'>How are you?</p><br><br>", //Sup
+    "<br><br>What's Up? <span class='hand'>👋</span><p style='color: beige;font-size: 30%;font-weight: 400;'>How are you?</p><br><br>", // What's up?
+    "<br><br>Hiya! <span class='hand'>👋</span><p style='color: beige;font-size: 30%;font-weight: 400;'>How are you?</p><br><br>", //Hiya!
+    "<br><br>Ahoy! <span class='hand'>👋</span><p style='color: beige;font-size: 30%;font-weight: 400;'>How are you?</p><br><br>", //Ahoy!
+    "<br><br>Howdy! <span class='hand'>👋</span><p style='color: beige;font-size: 30%;font-weight: 400;'>How are you?</p><br><br>", //Howdy!
+    "<br><br>‘Ello, gov’nor! <span class='hand'>👋</span><p style='color: beige;font-size: 30%;font-weight: 400;'>How are you?</p><br><br>" // ‘Ello, gov’nor!
 ];
 
 
