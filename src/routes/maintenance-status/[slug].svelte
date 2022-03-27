e4c93a683e5cfbc1702d9161b178ccdea81205cc
Mawoka <mawoka@e.email>
2022-03-27 18:19:12 +0200

    Added maintenance-status

---
+++ src/routes/maintenance-status/[slug].svelte
@@ -0,0 +1,42 @@
+<script context="module">
+	import json_data from './status.json';
+	export const load = async ({ params, fetch }) => {
+		if (json_data[params.slug] === undefined) {
+			return {
+				status: 404
+			};
+		}
+		return {
+			props: {
+				status_id: params.slug
+			}
+		};
+	};
+</script>
+
+<script lang="ts">
+  import Maintenance_status_lists from './maintenance-status-lists.svelte';
+
+	export let status_id: string;
+
+	interface maintain_statatus {
+		title: String;
+		fix_bugs: Boolean;
+		fix_security_issues: Boolean;
+		new_features: Boolean;
+		revivie_if_interested: Boolean;
+	}
+
+	const data: maintain_statatus = json_data[status_id];
+</script>
+
+<div>
+	<section class="section">
+		<div class="container pt-12 text-center">
+			<h1 class="text-8xl mt-6 marck-script">{data.title}</h1>
+		</div>
+	</section>
+	<section>
+		<Maintenance_status_lists data={data} />
+	</section>
+</div>
---
+++ src/routes/maintenance-status/index.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import MaintenanceStatusLists from './maintenance-status-lists.svelte';
+	import json_data from './status.json';
+
+
+	const maintain_statuses_ids = Object.entries(json_data);
+	console.log(maintain_statuses_ids);
+</script>
+
+<div>
+	<section class="section">
+		<div class="pt-12">
+			<h1 class="text-8xl mt-6 marck-script text-center">Maintenance-Status</h1>
+		</div>
+	</section>
+	<section class="w-fit flex flex-col mx-auto">
+		{#each maintain_statuses_ids as id}
+			{@const data = json_data[id[0]]}
+			<div class="p-8">
+				<a href="/maintenance-status/{id[0]}">
+					<div class="border p-8">
+						<h3 class="text-3xl text-center">Maintenance-Status ID: {id[0]}</h3>
+						<h4 class="text-2xl text-center pt-2">Title: {data.title}</h4>
+						<MaintenanceStatusLists {data} />
+					</div>
+				</a>
+			</div>
+		{/each}
+	</section>
+</div>
